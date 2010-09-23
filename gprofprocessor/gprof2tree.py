cbd54a2687422567a35f5c577cbcab110fbf6ff8
Tomoya Kabe <kabe@logos.ic.i.u-tokyo.ac.jp>
2010-09-24 02:36:56 +0900

    find descendants

--- gprofprocessor/gprof2tree.py
+++ gprofprocessor/gprof2tree.py
@@ -4,38 +4,58 @@ import sys
 
 import gprof2dot.gprof2dot
 
-def descendants(parser, f):
-    if not f.children: return []
-    print "Traversing %s" % (f.name)
-    li = [parser.functions[g.index] for g in f.children]
-    for fx in f.children:
-        if fx.cycle: continue
-        fobj = parser.functions[fx.index]
-        li.extend([g for g in descendants(parser, fobj)])
-    return li
 
-def select_funcs(parser) :
+def descendants(parser, f):
+    """list all descendants of f.
+
+    Parameters:
+    - parser: gprof2dot's parser
+    - f: function
+    """
+    if not f.children:
+        return []
+    retlist = []
+    #q = [parser.functions[g.index] for g in f.children]
+    q = [f]
+    #retlist.extend([f for f in q if not f.cycle])
+    while q:
+        g = q.pop()
+        cs = [parser.functions[h.index] for h in g.children
+                        if not parser.functions[h.index] in retlist]
+        retlist.extend(cs)
+        q.extend(cs)
+    return retlist
+
+
+def select_funcs(parser):
     """Select Functions to output as self time and cumulative time.
-    
+
     Arguments:
     - `parser`:
     """
-    s = []
-    c = []
+    s = []  # self functions
+    c = []  # cumulative functions
+    r = []  # removed for cumulation functions
     # divide functions into s and c
     for k, v in parser.functions.iteritems():
-        if v.name.startswith("PMPI"):
-            c.append(v)
-        else :
+        if v.name == "main":  # time of main unnecessary
+            continue
+        try:
+            if v.name.startswith("PMPI") \
+                    or v.name.index("MPI_Init") >= 0:
+                c.append(v)
+            else:
+                s.append(v)
+        except ValueError:
             s.append(v)
-    # Remove functions who has an ancestor has the name starts with "PMPI"
-    #for f in c:
-    print [f.index for f in s if f.name == "daxpy"]
-    #print (f.name for f in descendants(parser, s[0]))
-    print descendants(parser, parser.functions[241])
-    return s, c
+    # Remove functions who is a descendant of any function in c
+    for cf in c:
+        for df in descendants(parser, cf):
+            r = [s.pop(i) for i, sf in enumerate(s) if sf.name == df.name]
+    return s, c, r
 
-def main() :
+
+def main():
     """
     """
     fp = sys.stdin
@@ -43,7 +63,8 @@ def main() :
         fp = open(sys.argv[1])
     parser = gprof2dot.gprof2dot.GprofParser(fp)
     parser.parse()
-    selffuncs, cumfuncs = select_funcs(parser)
+    selffuncs, cumfuncs, removedfuncs = select_funcs(parser)
+    print [f.name for f in selffuncs]
 
-if __name__ == '__main__' :
+if __name__ == '__main__':
     main()
