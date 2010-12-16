0e1543c73aad60a18e37b78e43beb5b817d410d2
Tomoya Kabe <kabe@logos.ic.i.u-tokyo.ac.jp>
2010-12-16 11:34:05 +0900

    New view: group exec merge

---
+++ sql/sqlite3/create_view_groupexecmerge.sql
@@ -0,0 +1,15 @@
+-- Merges multiple profexec and shows the average values for each profgroup
+
+CREATE VIEW groupexecmerge
+AS
+SELECT
+    frs.funcname,
+    pe.profgroup_id,
+    AVG(frs.rank_sum) exec_sum_avg,
+    AVG(frs.rank_max) exec_max_avg,
+    AVG(frs.rank_min) exec_min_avg,
+    AVG(frs.rank_avg) exec_avg_avg
+FROM funcranksum frs, profexec pe
+WHERE frs.profexec_id=pe.id
+GROUP BY pe.profgroup_id, frs.funcname
+;
