a557845b9fde932a6cebe9b39092b1a0004d5a05
Tomoya Kabe <kabe@logos.ic.i.u-tokyo.ac.jp>
2011-01-06 17:27:46 +0900

    prettify sql

--- sql/postgres/create_view_funcranksum.sql
+++ sql/postgres/create_view_funcranksum.sql
@@ -2,6 +2,11 @@
 -- View providing aggregations for each execution
 
 CREATE VIEW funcranksum AS
-SELECT funcname, profexec_id, SUM(excl) rank_sum, MAX(excl) rank_max, MIN(excl) rank_min, AVG(excl) rank_avg
+SELECT funcname,
+       profexec_id,
+       SUM(excl) rank_sum,
+       MAX(excl) rank_max,
+       MIN(excl) rank_min,
+       AVG(excl) rank_avg
 FROM funcprofile
 GROUP BY funcname, profexec_id;
