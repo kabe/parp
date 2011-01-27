edd41c0bd7b580796d4a208cbbbd23f63948ab70
Tomoya Kabe <kabe@logos.ic.i.u-tokyo.ac.jp>
2011-01-28 01:14:59 +0900

    admin view

--- parpview/urls.py
+++ parpview/urls.py
@@ -1,4 +1,7 @@
 from django.conf.urls.defaults import *
+from django.contrib import admin
+admin.autodiscover()
+
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -9,10 +12,10 @@ urlpatterns = patterns('',
     # (r'^parpview/', include('parpview.foo.urls')),
     (r'^$', 'parpview.viewer.views.helloworld'),
     (r'^ut/$', 'parpview.viewer.views.usetemplate'),
-    (r'^pgdiff/(?P<pg1>\d+)/(?P<pg2>\d+)/$', 'parpview.viewer.views.pgroupdiff')
+    (r'^pgdiff/(?P<pg1>\d+)/(?P<pg2>\d+)/$', 'parpview.viewer.views.pgroupdiff'),
     # Uncomment the admin/doc line below to enable admin documentation:
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # (r'^admin/', include(admin.site.urls)),
+    (r'^admin/', include(admin.site.urls)),
 )
