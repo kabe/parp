998e6a8aa67484bbe26ba2d9064066030aa00f8b
Tomoya Kabe <kabe@logos.ic.i.u-tokyo.ac.jp>
2011-01-29 18:31:09 +0900

    parameters

--- parpview/viewer/models.py
+++ parpview/viewer/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib import admin
 
 # Create your models here.
+
+class ViewParam(models.Model):
+    """Viewer Parameters.
+    """
+    DB_TYPES = (
+        ("sqlite3", "SQLite3"),
+        ("postgres", "PostgreSQL"),
+        )
+    susp_thresh = models.FloatField()
+    sqlite3_file = models.TextField()
+    dbtype = models.CharField(max_length=15, choices=DB_TYPES)
+
+admin.site.register(ViewParam)
