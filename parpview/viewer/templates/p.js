c1ba87d359341d83f1e5753884d668f0ec53a872
Tomoya Kabe <kabe@logos.ic.i.u-tokyo.ac.jp>
2011-03-05 04:55:10 +0900

    javascript based see-more/hide interface javascript based POST request link

--- parpview/viewer/templates/p.js
+++ parpview/viewer/templates/p.js
@@ -22,3 +22,35 @@ function makeIndex() {
     $('toc').innerHTML = str;
 }
 
+function seemorefunc() {
+    var hidden_trs = $$('tr.hiddentrs');
+    $('seemore_tr').hide();
+    $('hide_tr').show();
+    hidden_trs.each(Element.show);
+}
+
+function hidefunc() {
+    var hidden_trs = $$('tr.hiddentrs');
+    $('seemore_tr').show();
+    $('hide_tr').hide();
+    hidden_trs.each(Element.hide);
+}
+
+function submitform(colnum) {
+    var form = $('choiceform');
+    var condition = 'input[type="radio"]';
+    var radios = $('coldef_table').select(condition);
+    radios.each(function(x) {
+            if (x.hasAttribute('name')) {
+                if(x.readAttribute('name') == 'order') {
+                    if (x.hasAttribute('value')) {
+                        if(x.readAttribute('value') == colnum) {
+                            x.writeAttribute('checked', 'checked');
+                        }
+                    }
+                }
+            }
+        });
+    form.submit();
+    return false;
+}
