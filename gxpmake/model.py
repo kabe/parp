c37b769273e7f06622e9ee107de15e6e3c615179
Tomoya Kabe <kabe@logos.ic.i.u-tokyo.ac.jp>
2011-08-27 03:01:37 +0900

    record model

--- gxpmake/model.py
+++ gxpmake/model.py
@@ -30,8 +30,47 @@ class Worker():
         self.memory = memory
 
     def __repr__(self, ):
-        """
+        """Representation form.
+
+        Too long name will be abbreviated to first 10 characters.
         """
         name_repr = self.name if len(self.name) < 10 else self.name[:10] + "..."
         s = "<Worker instance index=%d ncpus=%d memory=%d name='%s'>"
         return s % (self.index, self.ncpus, self.memory, name_repr)
+
+
+class Record(object):
+    """Record of an application.
+    """
+
+    _dic = {}
+
+    def __getattr__(self, attr):
+        """Transaparent accessor to _dic
+        
+        @param attr attribute name
+        >>> r = Record(a="b", c="d")
+        >>> r.a
+        'b'
+        >>> r.c
+        'd'
+        >>> r.e
+        Traceback (most recent call last):
+        ...
+        KeyError: 'e'
+        """
+        return self._dic[attr]
+
+    def __init__(self, **dic):
+        """Constructor.
+
+        @param **dic dictionary of attributes
+        >>> r = Record(a="b", c="d")
+        """
+        for k, v in dic.iteritems():
+            self._dic[k] = v
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
