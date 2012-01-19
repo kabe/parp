93de1c832a3bd2613afe498cd350bd86f7cb8755
Tomoya Kabe <kabe@logos.ic.i.u-tokyo.ac.jp>
2012-01-19 20:53:03 +0900

    hosei pagerank script

--- util/register_hosei_pagerank.sh
+++ util/register_hosei_pagerank.sh
@@ -24,4 +24,10 @@ for x in $PROFS;do
     $CMD --place hosei --library "OpenMPI GCC NFS" --appname $APPNAME $PR_BASE/$x.hosei-charlie.intrigger.cis.k.hosei.ac.jp $PR_BASE/$MAP
 done
 
+MAP=pagerank_tau_openmpi.map
+PROFS="`seq 22366 22375`"
+for x in $PROFS;do
+    $CMD --place hosei --library "OpenMPI-t GCC NFS" --appname $APPNAME $PR_BASE/$x.hosei-charlie.intrigger.cis.k.hosei.ac.jp $PR_BASE/$MAP
+done
+
 
